14e906d1f851ebb39a8ecc0a73f17c48104ef7c4
Kenji Ohtsuka <kok.fdcm@gmail.com>
2019-03-27 23:11:10 +0900

    update

--- src/macosMain/kotlin/sample/SampleMacos.kt
+++ src/macosMain/kotlin/sample/SampleMacos.kt
@@ -1,7 +1,48 @@
 package sample
 
-fun hello(): String = "Hello, Kotlin/Native!"
+import platform.Foundation.*
+import platform.darwin.*
+import platform.posix.sleep
 
-fun main() {
-    println(hello())
+fun main(args: Array<String>) {
+    println(WebGateway.get("https://www.google.com/"))
+}
+
+object WebGateway {
+    fun get(urlString: String): String {
+        val semaphore = dispatch_semaphore_create(0)
+
+        val components = NSURLComponents(urlString)
+        //compnents?.queryItems = queryItems
+        val url = components.URL
+        var result: String = ""
+        val config = NSURLSessionConfiguration.defaultSessionConfiguration()
+        config.waitsForConnectivity = true
+        config.timeoutIntervalForResource = 300.0
+
+        val request = NSURLRequest.requestWithURL(url!!)
+        val session = NSURLSession.sessionWithConfiguration(
+            config, null, NSOperationQueue.mainQueue()
+        )
+
+        //val session = NSURLSession.sharedSession
+        println(urlString)
+        val task =
+            session.dataTaskWithRequest(request) { nsData: NSData?, nsurlResponse: NSURLResponse?, nsError: NSError? ->
+                nsData?.run { result = toString() }
+                println(nsData)
+                dispatch_semaphore_signal(semaphore);
+            }
+
+        task.resume()
+        session.finishTasksAndInvalidate()
+        NSOperationQueue.mainQueue().waitUntilAllOperationsAreFinished()
+        dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER)
+        //session.finishTasksAndInvalidate()
+        return result
+    }
+
+    fun post(url: String): String {
+        TODO("not implemented")
+    }
 }
